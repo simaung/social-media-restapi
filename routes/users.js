191d618043f4e9ebae0612370b79758a5fb4fce5
simaung <ddhr00@gmail.com>
2022-03-24 16:17:26 +0700

    process delete users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const User = require("../model/User");
 const bcrypt = require("bcrypt");
 
 router.put("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -20,6 +20,19 @@ router.put("/:id", async (req, res) => {
         }
     } else {
         return res.status(403).json("You can update only your account!")
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            return res.status(200).json("Account has been deleted");
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    } else {
+        return res.status(403).json("You can delete only your account!")
     }
 });
 
